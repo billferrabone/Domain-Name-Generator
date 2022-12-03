1543773781f81473d76e01eb9f6e291bad6e84bc
billferrabone <billferrabone@gmail.com>
2022-12-03 01:35:10 +0000

    el nuevo generador

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const pronoun = ["the", "our"];
+  const adj = ["great", "big"];
+  const noun = ["jogger", "racoon"];
+  let com = [".com", ".org", ".net"];
+  let domgen = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < com.length; l++) {
+          console.log(pronoun[i] + "" + adj[j] + "" + noun[k] + "" + com[l]);
+          domgen.push(pronoun[i] + "" + adj[j] + "" + noun[k] + "" + com[l]);
+        }
+      }
+    }
+  }
+  let ramdomdomain = domgen[Math.floor(Math.random() * domgen.length)];
+  document.getElementById("dominio").innerHTML = ramdomdomain;
+
+  //   //write your code here
+  //   console.log("Hello Rigo from the console!");
 };
